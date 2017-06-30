9ac197e6cb72077ab8b842116b15369082598ebb
maksimmarchyk <maksim-nitka2015@yandex.ru>
2017-06-30 08:42:33 +0300

    пробные изм

--- 1consoleapp/Program.cs
+++ 1consoleapp/Program.cs
@@ -6,9 +6,12 @@ using System.Threading.Tasks;
 
 namespace _1consoleapp
 {
-    //Первое задание //тест
+    //используя делегаты delegate void Del(string s) ;  и статический методы
+    //класса, которые выводят на консоль разные слова: martch, 25, 1917, may. составьте фразы и выведите их на консоль вызовом делегата
     class Program
     {
+        public delegate void Del(string s); 
+
         static void Main(string[] args)
         {
             int x; 
